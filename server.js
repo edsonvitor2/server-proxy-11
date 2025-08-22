b4a85c68b513dcf6c5cb08c0ab10e5fc8c5e08df
Edson Vitor de Sousa <82064658+edsonvitor2@users.noreply.github.com>
2025-08-22 16:15:30 -0300

    alterado para usar commomjs

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-import express from 'express';
-import fetch from 'node-fetch';
+const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
-const INTERNAL_SERVER = "http://192.168.20.171:3020";
+const INTERNAL_SERVER = "http://192.168.20.171:3040";
 
 app.use(express.json());
 
-app.all('/api/*', async (req, res) => {
+app.all("/api/*", async (req, res) => {
   try {
-    const url = `${INTERNAL_SERVER}${req.path.replace('/api', '')}`;
+    const url = `${INTERNAL_SERVER}${req.path.replace("/api", "")}`;
     const options = {
       method: req.method,
-      headers: { ...req.headers, host: undefined }, // tira host original
-      body: ['GET','HEAD'].includes(req.method) ? undefined : JSON.stringify(req.body)
+      headers: { ...req.headers, host: undefined }, // remove header host
+      body: ["GET", "HEAD"].includes(req.method)
+        ? undefined
+        : JSON.stringify(req.body),
     };
 
     const response = await fetch(url, options);
     const data = await response.text();
-    res.status(response.status).send(data);
 
+    res.status(response.status).send(data);
   } catch (err) {
     console.error("Erro no proxy:", err);
-    res.status(500).json({ error: "Erro ao redirecionar para o servidor interno" });
+    res
+      .status(500)
+      .json({ error: "Erro ao redirecionar para o servidor interno" });
   }
 });
 
